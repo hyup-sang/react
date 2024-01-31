03ba5eb8aadfe114d855657d3a7334bac238b22a
rtg <rtg1539@naver.com>
2024-01-31 21:09:40 +0900

    components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import LearningComponent from './components/learning-examples/LearningComponent';
 
 function App() {
   return (
     <div className="App">
-      <FirstComponent></FirstComponent>
-      <SecondComponent></SecondComponent>
+      <LearningComponent></LearningComponent>
     </div>
   );
-}
-
-function FirstComponent() {
-  return (
-    <div className="FirstComponent">First Component</div>
-  )
-}
-function SecondComponent() {
-  return (
-    <div className="SecondComponent">Second Component</div>
-  )
 }
 
 export default App;
