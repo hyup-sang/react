6c343ee8dbf3199aa55f28a5a7bac183ee5ff592
rtg <rtg1539@naver.com>
2024-02-01 15:33:59 +0900

    todo component

--- todo-app/src/components/todo/ListTodosComponent.jsx
+++ todo-app/src/components/todo/ListTodosComponent.jsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react"
-import { retrieveAllTodosForUsername } from "./api/TodoApiService"
+import { useNavigate } from "react-router-dom"
+import { retrieveAllTodosForUsernameApi, deleteTodoApi } from "./api/TodoApiService"
+import { useAuth } from "./security/AuthContext"
 
 function ListTodosComponent() {
     const today = new Date()
+    const authContext = useAuth()
+    const username = authContext.username
     const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay())
     const [todos,setTodos] = useState([])
+    const [message,setMessage] = useState(null)
     useEffect (() => refreshTodos(), [])
+    const navigate = useNavigate()
+
+    function updateTodo(id) {
+        navigate(`/todo/${id}`)
+    }
 
     function refreshTodos() {
-        retrieveAllTodosForUsername('hong')
+        retrieveAllTodosForUsernameApi(username)
         .then(response => {
             setTodos(response.data)
         })
@@ -16,17 +26,29 @@ function ListTodosComponent() {
     
     }
 
+    function deleteTodo(id) {
+        console.log('clicked ' + id)
+        deleteTodoApi(username, id)
+        .then(() => {
+            setMessage(`Delete of todo with id = ${id} successful`)
+            refreshTodos()
+        })
+        .catch(error => console.log(error))
+    }
+
     return (
         <div className="container">
             <h1>Things You Want To Do!</h1>
+            {message && <div className="alert alert-warning">{message}</div>}
             <div>
                 <table className="table">
                     <thead>
                             <tr>
-                                <td>ID</td>
-                                <td>Description</td>
-                                <td>Is Done?</td>
-                                <td>Target Date</td>
+                                <th>Description</th>
+                                <th>Is Done?</th>
+                                <th>Target Date</th>
+                                <th>Delete</th>
+                                <th>Update</th>
                             </tr>
                     </thead>
                     <tbody>
@@ -34,16 +56,27 @@ function ListTodosComponent() {
                         todos.map(
                             todo => (
                                 <tr key={todo.id}>
-                                    <td>{todo.id}</td>
                                     <td>{todo.description}</td>
                                     <td>{todo.done.toString()}</td>
                                     <td>{todo.targetDate.toString()}</td>
+                                    <td>
+                                        <button className="btn btn-warning" onClick={() => deleteTodo(todo.id)}>
+                                            Delete
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button 
+                                            className="btn btn-success" 
+                                            onClick={() => updateTodo(todo.id)}
+                                        >
+                                            Update
+                                        </button>
+                                    </td>
                                 </tr>
                             )
                         )
                     }
                     </tbody>
-
                 </table>
             </div>
         </div>
--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -5,6 +5,7 @@ import ListTodosComponent from './ListTodosComponent'
 import ErrorComponent from './ErrorComponent'
 import WelcomeComponent from './WelcomeComponent'
 import LoginComponent from './LoginComponent'
+import TodoComponent from './TodoComponent'
 import AuthProvider, { useAuth } from './security/AuthContext'
 
 import './TodoApp.css'
@@ -36,6 +37,12 @@ export default function TodoApp() {
                     <Route path='/todos' element={
                         <AuthenticatedRoute>
                             <ListTodosComponent /> 
+                        </AuthenticatedRoute>
+                    } />
+
+                    <Route path='/todo/:id' element={
+                        <AuthenticatedRoute>
+                            <TodoComponent /> 
                         </AuthenticatedRoute>
                     } />
 
