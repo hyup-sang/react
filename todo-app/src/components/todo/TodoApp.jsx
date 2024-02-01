4104ea369ea18df57cfb6bfc11a0f71bc57204d5
rtg <rtg1539@naver.com>
2024-02-01 11:14:03 +0900

    todo list

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import {BrowserRouter, Routes, Route, useNavigate, useParams} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, useNavigate, useParams, Link} from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp() {
@@ -7,10 +7,12 @@ export default function TodoApp() {
         <div className='TodoApp'>
             <BrowserRouter>
             <Routes>
-                <Route path='/' element={ <LoginComponent /> }></Route>
-                <Route path='/login' element={ <LoginComponent /> }></Route>
-                <Route path='/welcome/:username' element={<WelcomeComponent /> }></Route>
-                <Route path='*' element={<ErrorComponent /> }></Route>
+                <Route path='/' element={ <LoginComponent /> } />
+                <Route path='/login' element={ <LoginComponent /> } />
+                <Route path='/welcome/:username' element={<WelcomeComponent /> } />
+                <Route path='/todos' element={<ListTodosComponent /> } />
+
+                <Route path='*' element={<ErrorComponent /> } />
             </Routes>
             </BrowserRouter>
         </div>
@@ -76,7 +78,7 @@ function WelcomeComponent() {
         <div className="WelcomeComponent">
             <h1>Welcome {username}</h1>
             <div>
-                Welcome Component
+                Manage your todos - <Link to="/todos">Go here</Link>
             </div>
         </div>
     )
@@ -92,3 +94,46 @@ function ErrorComponent() {
         </div>
     )
 }
+
+function ListTodosComponent() {
+    const today = new Date()
+    const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay())
+    const todos = [
+        {id: 1, description: 'Learn AWS', done: false, targetDate:targetDate},
+        {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate:targetDate},
+        {id: 3, description: 'Learn DevOps', done: false, targetDate:targetDate},
+    ]
+    return (
+        <div className="ListTodosComponent">
+            <h1>Things You Want To Do!</h1>
+            <div>
+                <table>
+                    <thead>
+                            <tr>
+                                <td>ID</td>
+                                <td>Description</td>
+                                <td>Is Done?</td>
+                                <td>Target Date</td>
+                            </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        todos.map(
+                            todo => (
+                                <tr key={todo.id}>
+                                    <td>{todo.id}</td>
+                                    <td>{todo.description}</td>
+                                    <td>{todo.done.toString()}</td>
+                                    <td>{todo.targetDate.toDateString()}</td>
+                                </tr>
+                            )
+                        )
+                    }
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    )
+}
+
