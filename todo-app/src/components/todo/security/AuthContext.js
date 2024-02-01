dc07e2f4e8ec75214f2bb51cea811c57ee7d7ce5
rtg <rtg1539@naver.com>
2024-02-01 18:21:22 +0900

    jwt token

--- todo-app/src/components/todo/security/AuthContext.js
+++ todo-app/src/components/todo/security/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { executeBasicAuthenticationService } from "../api/HelloWorldApiService";
+import { executeJwtAuthenticationService } from "../api/AuthenticationApiService";
 import { apiClient } from "../api/ApiClient";
 
 export const AuthContext = createContext()
@@ -12,20 +12,20 @@ export default function AuthProvider({ children }) {
     const [token, setToken] = useState(null)
 
     async function login(username, password) {
-        const baToken = 'Basic ' + window.btoa(username + ":" + password)
-
         try {
-            const response = await executeBasicAuthenticationService(baToken)
+            const response = await executeJwtAuthenticationService(username, password)
 
             if (response.status === 200) {
+                const jwtToken = `Bearer ${response.data.token}`
+
                 setAuthenticated(true)
                 setUsername(username)
-                setToken(baToken)
+                setToken(jwtToken)
 
                 apiClient.interceptors.request.use(
                     (config) => {
                         console.log('intercepting and adding a token')
-                        config.headers.Authorization = baToken
+                        config.headers.Authorization = jwtToken
                         return config
                     }
                 )
